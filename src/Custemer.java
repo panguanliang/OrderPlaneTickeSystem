d06b1c0295b33730624320b79fb348a40320e3a5
Administrator <1846201952@qq.com>
2021-01-11 15:13:29 +0800

    1、创建乘客类

---
+++ src/Custemer.java
@@ -0,0 +1,38 @@
+public class Custemer {
+    private String id;
+    private String name;
+    private String phone;
+    private String cardid;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCardid() {
+        return cardid;
+    }
+
+    public void setCardid(String cardid) {
+        this.cardid = cardid;
+    }
+}
